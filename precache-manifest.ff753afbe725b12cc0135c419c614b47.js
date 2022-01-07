d55961e415813809afb5dd821f697a801638c0a4
sadaigm <sadaigm@gmail.com>
2022-01-07 22:02:44 +0530

    Updates

---
+++ precache-manifest.ff753afbe725b12cc0135c419c614b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad3e15ccfefaadd67304fa61e60f1df",
+    "url": "/web-slate/index.html"
+  },
+  {
+    "revision": "f71ba3c97cc4003cb77b",
+    "url": "/web-slate/static/css/2.d52d6e90.chunk.css"
+  },
+  {
+    "revision": "498b2d4c6104ecca491e",
+    "url": "/web-slate/static/css/main.a9062e30.chunk.css"
+  },
+  {
+    "revision": "f71ba3c97cc4003cb77b",
+    "url": "/web-slate/static/js/2.7abb745c.chunk.js"
+  },
+  {
+    "revision": "308d2d97fd519422d20953c3e51f94e3",
+    "url": "/web-slate/static/js/2.7abb745c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498b2d4c6104ecca491e",
+    "url": "/web-slate/static/js/main.ef6af49e.chunk.js"
+  },
+  {
+    "revision": "60b51f6068586dfec130",
+    "url": "/web-slate/static/js/runtime-main.9f26c6d1.js"
+  }
+]);
